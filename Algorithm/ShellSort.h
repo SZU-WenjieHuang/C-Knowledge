6cbc84a169a59ed5b7ddf74613b8c6bfb66af30c
WENJIE HUANG <82434538+SZU-WenjieHuang@users.noreply.github.com>
2023-05-07 14:31:50 +0100

    Update ShellSort.h

--- Algorithm/ShellSort.h
+++ Algorithm/ShellSort.h
@@ -1,16 +1,26 @@
-// 希尔排序：每一轮按照事先决定的间隔进行插入排序，间隔会依次缩小，最后一次一定要是1。
-template<typename T>
-void shell_sort(T array[], int length) {
-    int h = 1;
-    while (h < length / 3) {
-        h = 3 * h + 1;
-    }
-    while (h >= 1) {
-        for (int i = h; i < length; i++) {
-            for (int j = i; j >= h && array[j] < array[j - h]; j -= h) {
-                std::swap(array[j], array[j - h]);
-            }
+class Solution {
+public:
+    /*
+    希尔排序：每一轮按照事先决定的间隔进行插入排序，间隔会依次缩小，最后一次一定要是1。
+    */
+
+    //希尔排序
+    vector<int> sortArray(vector<int>& nums) {
+        int len = nums.size();
+        // Get the Gap
+        int gap = 1;
+        while (gap < len / 3){ //保证整个数组至少被分成三份
+            gap = 3 * gap + 1;
         }
-        h = h / 3;
+
+        while (gap >= 1){
+            for(int i = gap; i < len; i++){ //从第二组开始向前比较
+                for(int j = i; j >= gap && nums[j] < nums[j-gap] ; j -= gap){
+                    swap(nums[j], nums[j - gap]);
+                }
+            }
+            gap = gap / 3;
+        }
+        return nums;
     }
-}
+};
